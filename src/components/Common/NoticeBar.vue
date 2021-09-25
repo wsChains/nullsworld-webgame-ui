a844b798a90d8983cd492a5fef374101fe607bd5
purepeace-0x <purepeace@0x11.email>
2021-09-25 16:09:58 +0800

    Refactor NoticeBar

--- src/components/Common/NoticeBar.vue
+++ src/components/Common/NoticeBar.vue
@@ -2,12 +2,8 @@
   <div class="announce-bar">
     <img src="/notice.png" />
     <div class="item-notice-content">
-      <ul class="list">
-        <li
-          v-for="(item, index) in ulist"
-          :key="item.id"
-          :class="!index && play ? 'toUp' : ''"
-        >{{ item.msg }}</li>
+      <ul :class="play ? 'animation-notice' : ''">
+        <li v-for="(item, index) in dataSource" :key="index">{{ item.msg }}</li>
       </ul>
     </div>
   </div>
@@ -18,45 +14,51 @@ import { formatNumber } from '@/utils/common'
 
 export default {
   mounted() {
-    setInterval(this.startPlay, 5000)
+    setInterval(this.startPlay, 2000)
   },
   data() {
     return {
       formatNumber,
-      ulist: [
+      dataSource: [
         { msg: `Player 0645... .04b68 won ${formatNumber(1234)} USDT in arena #12345` },
         { msg: `Player 1234... .v04g1 won ${formatNumber(233)} USDT in arena #43210` },
-        { msg: `Player 0xbz... .gg445 won ${formatNumber(666)} USDT in arena #44221` },
-        { msg: `Player 6543... .ds234 won ${formatNumber(888)} USDT in arena #12111` }
       ],
       play: false
     }
   },
   methods: {
     startPlay() {
-      let that = this
-      that.play = true
+      this.play = true
       setTimeout(() => {
-        that.ulist.push(that.ulist[0])
-        that.ulist.shift()
-        that.play = false
-      }, 5000)
+        this.dataSource.push(this.dataSource[0])
+        this.dataSource.shift()
+        this.play = false
+      }, 500)
     }
   }
 }
 </script>
 <style scoped>
 .item-notice-content {
-  overflow: hidden;
-  width: 400px;
+  width: 433px;
   background: #fef8de;
   display: flex;
   align-items: center;
+  padding: 2px 12px;
+  z-index: 1;
+  position: absolute;
+  top: 4px;
+  height: 24px;
+  left: 30px;
+  background: #fef8dee6;
+  border-radius: 0px 10px 10px 0px;
+  overflow: hidden;
 }
 
 .announce-bar {
-  height: 40px;
+  height: 33px;
   position: relative;
+  overflow: hidden;
 }
 
 .announce-bar img {
@@ -66,36 +68,20 @@ export default {
   width: 33px;
 }
 
-.announce-bar div {
-  padding: 2px 12px;
-  z-index: 1;
-  position: absolute;
-  top: 4px;
-  left: 30px;
-  background: #fef8dee6;
-  border-radius: 0px 10px 10px 0px;
-}
-
-.toUp {
-  margin-left: -433px;
-  transition: all 5s;
+.animation-notice {
+  transform: translateY(-22px);
+  transition: transform 0.5s;
 }
 
 ul {
-  display: flex;
-}
-
-li {
-  width: 433px;
-  text-align: left;
-  white-space: nowrap;
-}
-
-.list {
-  list-style: none;
-  text-align: center;
+  margin-top: 22px;
   padding: 0;
-  margin: 0;
-  align-items: center;
+  list-style: none;
+}
+
+ul li {
+  width: 433px;
+  line-height: 22px;
+  height: 22px;
 }
 </style>
