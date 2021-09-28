4634308b5629951f806af3aeac4e86fb4b9f2ea1
PurePeace-0x <purepeace@0x11.email>
2021-09-28 17:13:01 +0800

    Refactor Wallet Store

--- src/stores/wallet.js
+++ src/stores/wallet.js
@@ -1,11 +1,14 @@
-import { toRaw } from 'vue'
+import { ref, markRaw } from 'vue'
 import { defineStore } from 'pinia'
 import { Contract } from 'ethers'
 
+import { ERC20 } from '@/contracts'
 
 import { cutEthAddress } from '@/utils/common'
-import { CHAIN_ID, CHAIN_ID_HEX, tryNewWalletWatcher, errInfo, findEvent } from '@/utils/wallet'
-import { ERC20 } from '@/contracts'
+import { WalletWatcher } from '@/utils/wallet/watcher'
+import { Metamask, WalletConnect } from '@/utils/wallet/connectors'
+import { findEvent } from '@/utils/wallet/utils'
+import { CHAIN_ID } from '@/utils/wallet/constants'
 
 
 export const useWallet = (app) => {
@@ -16,9 +19,10 @@ export const useWallet = (app) => {
             /** @type {string | null} */
             address: null,
             chainId: null,
-            /** @type { import('@/utils/wallet').WalletWatcher | null } **/
-            $: null,
-            _signer: null
+            /** @type { import('@/utils/wallet/watcher').WalletWatcher | null } **/
+            $: markRaw({}),
+            signer: markRaw({}),
+            connectedWallet: null
         }),
         getters: {
             currentAccount(state) {
@@ -32,32 +36,37 @@ export const useWallet = (app) => {
             },
             isCorrectNetwork(state) {
                 return state.chainId === CHAIN_ID
-            },
-            signer(state) {
-                return toRaw(state._signer)
-            },
-            noWallet() {
-                return !!window?.ethereum
-            },
-            connectedWallet(state) {
-                return state.$?.connector?.label
             }
         },
         actions: {
             /** @param {string} address **/
             add(address) {
                 address = address.toLocaleLowerCase()
-                this.accountList[address] = { address }
+                this.accountList[address] = address
                 this.address = address
             },
-            disconnect() {
-                this.address = undefined
+            clearStore() {
+                this.accountList[this.address] = undefined
+                this.address = null
+                this.chainId = null
+                this.$ = markRaw({})
+                this.signer = markRaw({})
+                this.connectedWallet = null
+            },
+            async tryDisconnect() {
+                if (this.$?.connector?.disconnect) {
+                    await this.$?.connector?.disconnect()
+                    this.clearStore()
+                    this.init()
+                }
             },
             setChainId(chainId) {
+                console.log('[Wallet Store] setChainId')
                 this.chainId = Number(chainId)
             },
             setSigner(signer) {
-                this._signer = signer
+                console.log('[Wallet Store] setSigner')
+                this.signer = markRaw(signer)
             },
             createContract(contract) {
                 if (!this.signer) return
@@ -67,39 +76,64 @@ export const useWallet = (app) => {
                 if (!this.signer) return
                 return new Contract(tokenAddress, ERC20.abi, this.signer)
             },
-            async init({ connector, forceConnect } = {}) {
-                if (!forceConnect && !window?.ethereum?.selectedAddress) return false
-                const callback = (...ev) => {
-                    if (!ev) return
-                    const disconnect = findEvent(ev, 'disconnect')
-                    const accountsChanged = findEvent(ev, 'accountsChanged')
-                    const chainChanged = findEvent(ev, 'chainChanged')
-
-                    if (disconnect) this.disconnect()
-                    if (accountsChanged?.data?.length === 0) this.disconnect()
-                    else if (accountsChanged?.data?.length) this.add(accountsChanged.data[0])
-                    if (chainChanged) this.setChainId(chainChanged.data)
+            async init({ connector } = {}) {
+                if (!connector) {
+                    if (window?.ethereum?.selectedAddress) {
+                        connector = new Metamask()
+                    } else {
+                        connector = new WalletConnect()
+                    }
                 }
-                const { wallet, err } = await tryNewWalletWatcher(connector, callback)
-                if (err) {
-                    app.config.globalProperties.$message.error(errInfo(err))
-                    return false
-                }
+                if (!connector) return
 
-                this.$ = wallet
-                const { signer, signerAddress } = await wallet.getSigner()
-                this.setChainId(wallet.chainIdNumber)
+                if (!await this.handleWalletConnect(connector)) return
+                this.setChainId(await this.$.chainIdNumber())
+                if (this.connectedWallet === 'MetaMask') await this.switchNetwork()
+
+                return await this.updateSigner()
+            },
+            async updateSigner() {
+                console.info('[Wallet Store] updateWalletData start.')
+                const { signer, signerAddress } = await this.$.getSigner()
                 this.add(signerAddress)
                 this.setSigner(signer)
+                console.info('[Wallet Store] updateWalletData done.')
+                return true
+            },
+            walletWatchCallback(...ev) {
+                if (!ev) return
+                console.log('[Wallet Store] Event: ', ev)
+                const disconnect = findEvent(ev, 'disconnect')
+                const accountsChanged = findEvent(ev, 'accountsChanged')
+                const chainChanged = findEvent(ev, 'chainChanged')
+
+                if (disconnect) this.clearStore()
+                if (accountsChanged?.data?.length === 0) this.clearStore()
+                else if (accountsChanged?.data?.length) this.add(accountsChanged.data[0])
+                if (chainChanged) this.setChainId(chainChanged.data)
+            },
+            async handleWalletConnect(connector) {
+                try {
+                    console.info('[Wallet Store] Wallet connect start.')
+                    this.$ = markRaw(await new WalletWatcher(connector).connect({ cb: this.walletWatchCallback }))
+                    this.connectedWallet = ref(this.$?.connector?.label)
+                } catch (connectErr) {
+                    console.error('[Wallet Store] connectErr: ', connectErr)
+                    app.config.globalProperties.$message.error(connectErr.message)
+                    return false
+                }
+                console.info('[Wallet Store] Wallet connect done.')
                 return true
             },
             async switchNetwork() {
-               this.$.connector.request({
-                    method: 'wallet_switchEthereumChain',
-                    params: [{ chainId: CHAIN_ID_HEX }],
-                }).catch(err => {
-                    app.config.globalProperties.$message.error(err.message)
-                })
+                try {
+                    console.info('[Wallet Store] Chain switch start.')
+                    if (this.$.chainIdNumber() !== CHAIN_ID) await this.$.switchNetwork()
+                } catch (switchErr) {
+                    console.error('[Wallet Store] switchErr: ', switchErr)
+                    app.config.globalProperties.$message.error(switchErr.message)
+                }
+                console.info('[Wallet Store] Chain switch end.')
             }
         }
     })()
